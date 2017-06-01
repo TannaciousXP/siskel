3b43a1c0439344f39dcbdac15ca110c61c5b8912
Hack Reactor Student <student@hackreactor.com>
2017-05-31 17:13:24 -0700

    Completed siskel

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,49 @@
+/*
+"add" (model, collection, options) — when a model is added to a collection.
+"remove" (model, collection, options) — when a model is removed from a collection.
+"update" (collection, options) — single event triggered after any number of models have been added or removed from a collection.
+"reset" (collection, options) — when the collection's entire contents have been reset.
+"sort" (collection, options) — when the collection has been re-sorted.
+"change" (model, options) — when a model's attributes have changed.
+"change:[attribute]" (model, value, options) — when a specific attribute has been updated.
+"destroy" (model, collection, options) — when a model is destroyed.
+"request" (model_or_collection, xhr, options) — when a model or collection has started a request to the server.
+"sync" (model_or_collection, response, options) — when a model or collection has been successfully synced with the server.
+"error" (model_or_collection, response, options) — when a model's or collection's request to the server has failed.
+"invalid" (model, error, options) — when a model's validation fails on the client.
+"route:[name]" (params) — Fired by the router when a specific route is matched.
+"route" (route, params) — Fired by the router when any route has been matched.
+"route" (router, route, params) — Fired by history when any route has been matched.
+"all" — this special event fires for any triggered event, passing the event name as the first argument followed by all trigger arguments.
+
+---------------------------------------------------------------------------
+
+var Chapter  = Backbone.Model;
+var chapters = new Backbone.Collection;
+
+chapters.comparator = 'page';
+
+chapters.add(new Chapter({page: 9, title: "The End"}));
+chapters.add(new Chapter({page: 5, title: "The Middle"}));
+chapters.add(new Chapter({page: 1, title: "The Beginning"}));
+
+alert(chapters.pluck('title'));
+
+
+*/
+
 var Movie = Backbone.Model.extend({
 
   defaults: {
     like: true
   },
 
-  toggleLike: function() {
-    // your code here
+  toggleLike: function() {  
+
+    // console.log(this.get('like'));
+    var isLike = this.get('like');
+    isLike = !isLike;
+    this.set('like', isLike);
   }
 
 });
@@ -16,12 +54,17 @@ var Movies = Backbone.Collection.extend({
 
   initialize: function() {
     // your code here
+    this.on('change', function() {
+      this.sortByField();
+    });
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
     // your code here
+    this.comparator = field || this.comparator;
+    this.sort();  
   }
 
 });
@@ -59,6 +102,7 @@ var MovieView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
+    this.model.on('change', this.render, this);
   },
 
   events: {
@@ -67,6 +111,7 @@ var MovieView = Backbone.View.extend({
 
   handleClick: function() {
     // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -80,6 +125,7 @@ var MoviesView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
+    this.collection.on('sort', this.render, this);
   },
 
   render: function() {
